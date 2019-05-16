23ed93d41189c5e238a5fc34b44de34466fd7683
romina sejas <romina.v.sejas@gmail.com>
2019-05-16 09:24:01 -0400

    discription/message you want

---
+++ src/main/java/com/example/demo/Job.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 4)
+    private String title;
+
+    @NotNull
+    @Size(min = 3)
+    private String employer;
+
+    @NotNull
+    @Size(min = 10)
+    private String description;
+
+}
